8ef5f53bff57fdcd4e2229ec7d1433ddc6fcf4c6
Christyan Simbolon <sambungdata@gmail.com>
2026-07-20 20:33:56 +0700

    feat-backend: add app config

---
+++ backend/config/urls.py
@@ -0,0 +1,18 @@
+# =============================================================================
+# === backend/config/urls.py ===
+# =============================================================================
+from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+from django.urls import include, path
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+
+    path("api/auth/",          include("apps.authentication.urls")),
+    path("api/organizations/", include("apps.organizations.urls")),
+    path("api/",               include("apps.service.urls")),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
